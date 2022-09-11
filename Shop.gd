92230ea184f2d101be7885f2dcca312eee68e2bb
10xyz <82887902+10xyz-coder@users.noreply.github.com>
2022-09-11 20:00:02 +0530

    New Files

--- Shop.gd
+++ Shop.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-signal replay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,13 +11,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Exit_pressed():
-	# Go Back
-	get_tree().change_scene("res://Main_Menu.tscn")
-
-
-func _on_Play_pressed():
-	# Replay
-	emit_signal("replay")
