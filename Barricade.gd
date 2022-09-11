78bffb7d07c77c9a5f9b4bba8f9b4dbd4c909ab7
10xyz <82887902+10xyz-coder@users.noreply.github.com>
2022-09-11 19:52:55 +0530

    Update Barricade.gd

--- Barricade.gd
+++ Barricade.gd
@@ -11,7 +11,7 @@ func _ready():
 func _physics_process(delta):
 	# Move left by gamespeed
 	
-	if get_parent().gameSpeed == 0:
+	if get_parent().gameSpeed == 0 or position.x < -32:
 		queue_free()
 	position.x -= get_parent().gameSpeed
 
@@ -19,5 +19,17 @@ func _physics_process(delta):
 # Old Falling Down Code
 
 func _on_Area2D_area_exited(area):
+	pass
+	#if area.is_in_group("ladder"):
+		#get_parent().emit_signal("down")
+
+
+func _on_Area2D_area_entered(area):
+	pass
 	if area.is_in_group("ladder"):
-		get_parent().emit_signal("down")
+		$Timer.wait_time = 0.75  / (0.01+get_parent().gameSpeed)
+		$Timer.start()
+
+
+func _on_Timer_timeout():
+	get_parent().emit_signal("down")
